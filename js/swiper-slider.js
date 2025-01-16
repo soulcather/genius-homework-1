8286f20834d167f2b02fbe095aa0bf59ec3e195c
Yegor Sawinkov <mubest1701@gmail.com>
2025-01-16 21:26:34 +0200

    Added adaptive + slider on swiper.js

---
+++ js/swiper-slider.js
@@ -0,0 +1,29 @@
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  spaceBetween: 16,
+  slidesPerGroup: 3,
+  direction: 'horizontal',
+  loop: true,
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1280: {
+      slidesPerView: 3,
+    },
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    bulletClass: 'pagination__button',
+    bulletActiveClass: 'pagination__button--active',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.carousel-button.next',
+    prevEl: '.carousel-button.prev',
+  },
+});
