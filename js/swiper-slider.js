4da4c1ce06d656bc1ac66d4a34a117738eb66ccc
Yegor Sawinkov <mubest1701@gmail.com>
2025-01-17 14:49:38 +0200

    fix slider

--- js/swiper-slider.js
+++ js/swiper-slider.js
@@ -1,16 +1,18 @@
 const swiper = new Swiper('.swiper', {
   slidesPerView: 1,
   spaceBetween: 16,
-  slidesPerGroup: 3,
+  slidesPerGroup: 1,
   direction: 'horizontal',
   loop: true,
 
   breakpoints: {
     768: {
       slidesPerView: 2,
+      slidesPerGroup: 2,
     },
     1280: {
       slidesPerView: 3,
+      slidesPerGroup: 3,
     },
   },
 
